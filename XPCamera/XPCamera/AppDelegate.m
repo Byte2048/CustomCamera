41cb2ac473b0fe9e84d4af4c4d38baee6ff4bf27
Byte2048 <451497088@qq.com>
2019-11-21 10:59:07 +0800

    Update AppDelegate.m
    测试一下

--- XPCamera/XPCamera/AppDelegate.m
+++ XPCamera/XPCamera/AppDelegate.m
@@ -16,7 +16,7 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    //测试一下
     return YES;
 }
 
